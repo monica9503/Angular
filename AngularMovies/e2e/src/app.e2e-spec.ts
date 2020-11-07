6fff6fc31ec76bcaecfb1c248933a6f108e44846
monicachandni <monicachandni95@gmail.com>
2020-11-07 10:44:18 -0700

    fix

--- AngularMovies/e2e/src/app.e2e-spec.ts
+++ AngularMovies/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angular-movies app is running!');
+    expect(page.getTitleText()).toEqual('AngularMovies app is running!');
   });
 
   afterEach(async () => {
